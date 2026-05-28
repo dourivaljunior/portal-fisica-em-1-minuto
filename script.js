76555a2f20f2ed9ea4238991b7bc73cdf3792d64
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2026-05-28 21:42:18 +0000

    Refactor mouse effect and remove particle animation
    Refactor mouse effect on hero element for smoother interaction and remove particle animation code.

--- script.js
+++ script.js
@@ -2,85 +2,39 @@
 /* script.js */
 /* ========================= */
 
+/* ABRIR PÁGINAS */
+
 function openPage(page){
 
   window.location.href = page;
 }
+
+/* VOLTAR */
 
 function goBack(){
 
   window.history.back();
 }
 
-/* efeito mouse */
+/* EFEITO LEVE NO HERO */
 
 document.addEventListener("mousemove",(e)=>{
 
-  const hero = document.querySelector(".hero-box");
+  const hero =
+  document.querySelector(".hero-box");
 
-  const x = (window.innerWidth / 2 - e.pageX)/40;
+  if(hero){
 
-  const y = (window.innerHeight / 2 - e.pageY)/40;
+    const x =
+    (window.innerWidth / 2 - e.pageX) / 60;
 
-  hero.style.transform =
-  `rotateY(${x}deg) rotateX(${y}deg)`;
+    const y =
+    (window.innerHeight / 2 - e.pageY) / 60;
+
+    hero.style.transform =
+    `
+    rotateY(${x}deg)
+    rotateX(${y}deg)
+    `;
+  }
 });
-
-/* partículas douradas */
-
-const bg = document.querySelector(".background-animation");
-
-for(let i=0;i<40;i++){
-
-  const particle = document.createElement("div");
-
-  particle.classList.add("particle");
-
-  particle.style.position = "absolute";
-
-  particle.style.width = "4px";
-
-  particle.style.height = "4px";
-
-  particle.style.background = "gold";
-
-  particle.style.borderRadius = "50%";
-
-  particle.style.left = Math.random()*100+"%";
-
-  particle.style.top = Math.random()*100+"%";
-
-  particle.style.opacity = Math.random();
-
-  particle.style.animation = `
-  floatParticle
-  ${5 + Math.random()*10}s
-  linear infinite`;
-
-  bg.appendChild(particle);
-}
-
-/* css dinâmica */
-
-const style = document.createElement("style");
-
-style.innerHTML = `
-@keyframes floatParticle{
-
-  0%{
-    transform:translateY(0px);
-    opacity:0;
-  }
-
-  50%{
-    opacity:1;
-  }
-
-  100%{
-    transform:translateY(-100px);
-    opacity:0;
-  }
-}
-`;
-
-document.head.appendChild(style);
